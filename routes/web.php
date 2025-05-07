1afd09ac86400781aa73efe7ea76f2b848171273
Euisy Nayla <d1041221038@student.untan.ac.id>
2025-05-07 11:13:26 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,4 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('beranda');
 });
+
+// Menampilkan halaman login (GET request)
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login.form');
+
+// Menangani proses login (POST request)
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+// Menampilkan dashboard setelah login (GET request)
+Route::get('/dashboard', [AuthController::class, 'dashboard'])->name('dashboard');
+
 ?>
